6c0f0fec45c81247cd995cc108437f109df39f8f
Jed Smit <jedsmit1@gmail.com>
2020-07-19 14:25:52 -0700

    add palettelist and Links

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
+import PaletteList from './PaletteList';
 
 
 class App extends Component {
@@ -17,13 +18,17 @@ class App extends Component {
     console.log(generatePalette(seedColors[4]))
     return (
       <Switch>
-        <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>} />
-        <Route exact path="/palette/:id" render={routeProps => (
-          <Palette
-            palette={generatePalette(this.findPalette(routeProps.match.params.id)
-            )}
-          />
-        )} />
+        <Route exact path="/"
+          render={() => <PaletteList palettes={seedColors} />}
+        />
+        <Route exact path="/palette/:id"
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
       </Switch>
       // <div>
       //   <Palette palette={generatePalette(seedColors[4])} />
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import Snackbar from '@material-ui/core/Snackbar';
@@ -31,7 +32,7 @@ export class Navbar extends Component {
         return (
             <header className="Navbar">
                 <div className="logo">
-                    <a href='#'>reactpalette</a>
+                    <Link to="/">reactpalette</Link>
                 </div>
                 <div className="slider-container">
                     <span>Level: {level}</span>
