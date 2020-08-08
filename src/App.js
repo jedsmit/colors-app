2103cea2ca7813efe02ceb02616d667768821b38
Jed Smit <jedsmit1@gmail.com>
2020-08-08 15:55:47 -0700

    add singleColorPalette and link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from "react-router-dom";
 import Palette from './Palette';
+import SingleColorPalette from './SingleColorPalette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 import PaletteList from './PaletteList';
@@ -29,7 +30,7 @@ class App extends Component {
             />
           )}
         />
-        <Route exact path="/palette/:paletteId/:colorId" render={() => <h1>single color page</h1>} />
+        <Route exact path="/palette/:paletteId/:colorId" render={() => <SingleColorPalette />} />
       </Switch>
       // <div>
       //   <Palette palette={generatePalette(seedColors[4])} />
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -17,7 +17,7 @@ export class ColorBox extends Component {
     }
 
     render() {
-        const { name, background } = this.props;
+        const { name, background, moreUrl } = this.props;
         const { copied } = this.state
         return (
 
@@ -35,12 +35,12 @@ export class ColorBox extends Component {
                         </div>
                         <button className="copy-button">Copy</button>
                     </div>
-                    <Link to='/' onClick={e => e.stopPropagation}>
+                    <Link to={moreUrl} onClick={e => e.stopPropagation}>
                         <span className='see-more'>More</span>
                     </Link>
-                </div>
+                </div >
 
-            </CopyToClipboard>
+            </CopyToClipboard >
         )
     }
 }
