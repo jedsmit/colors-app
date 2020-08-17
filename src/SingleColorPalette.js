c34cedd06bbc6672f058f8de0e2c24e39ec013c4
Jed Smit <jedsmit1@gmail.com>
2020-08-17 13:39:40 -0700

    move out styles

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -4,49 +4,9 @@ import { withStyles } from "@material-ui/styles";
 import ColorBox from './ColorBox';
 import Navbar from './Navbar';
 import PaletteFooter from './PaletteFooter';
+import styles from './styles/PaletteStyles';
 import './ColorBox.css'
 
-
-const styles = {
-    Palette: {
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column"
-    },
-    colors: {
-        height: "90%"
-    },
-    goBack: {
-        width: "20%",
-        height: "50%",
-        margin: "0 auto",
-        display: "inline-block",
-        position: "relative",
-        cursor: "pointer",
-        marginBottom: "-3.5px",
-        opacity: 1,
-        backgroundColor: "black",
-        "& a": {
-            color: "white",
-            width: "100px",
-            height: "30px",
-            position: "absolute",
-            display: "inline-block",
-            top: "50%",
-            left: "50%",
-            marginLeft: "-50px",
-            marginTop: "-25px",
-            textAlign: "center",
-            outline: "none",
-            background: "rgba(255,255,255,0.3)",
-            fontSize: "1rem",
-            lineHeight: "30px",
-            textTransform: "uppercase",
-            border: "none",
-            textDecoration: "none",
-        },
-    }
-}
 
 
 export class SingleColorPalette extends Component {
