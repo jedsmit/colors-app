8275abcef1f1663f5b30169cfb2299bd14390677
Jed Smit <jedsmit1@gmail.com>
2020-08-19 17:14:12 -0700

    Add savePalette

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -90,6 +90,7 @@ class NewPaletteForm extends Component {
         this.updateCurrentColor = this.updateCurrentColor.bind(this);
         this.addNewColor = this.addNewColor.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     };
 
     componentDidMount() {
@@ -128,6 +129,16 @@ class NewPaletteForm extends Component {
         this.setState({ newName: e.target.value })
     }
 
+    handleSubmit() {
+        let newName = "New Test Palette";
+        const newPalette = {
+            paletteName: newName,
+            id: newName.toLowerCase().replace(/ /g, "-"),
+            colors: this.state.colors,
+        }
+        this.props.savePalette(newPalette);
+        this.props.history.push('/');
+    }
     render() {
         const { classes } = this.props;
         const { open } = this.state;
@@ -153,6 +164,7 @@ class NewPaletteForm extends Component {
                         <Typography variant="h6" noWrap>
                             Persistent drawer
           </Typography>
+                        <Button variant="contained" color="primary" onClick={this.handleSubmit}>Save Palette</Button>
                     </Toolbar>
                 </AppBar>
                 <Drawer
